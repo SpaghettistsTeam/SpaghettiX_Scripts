913425f4d1596c9b2409f05580b9fdcb44156961
SpaghettistsTeam <uloverrs@outlook.com>
2026-02-13 17:27:24 +0800

    Add auto mining script for ore collection

---
+++ BanbooCNfavoriteChineseGame.lua
@@ -0,0 +1,36 @@
+--Developed by SpaghettistsTeam
+getgenv().AutoMiningEnabled = true
+local Players = game:GetService("Players")
+local LocalPlayer = Players.LocalPlayer
+
+local OreFolder = workspace:WaitForChild("Ore")
+
+local function TelePort (pos)
+    if not pos then return end
+
+    LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = CFrame.new(pos)
+end
+
+local function AutoMining ()
+    for _, ore in ipairs(OreFolder:GetChildren()) do
+        if not getgenv().AutoMiningEnabled then break end
+
+        local Cube = ore:WaitForChild("Cube")
+        local ClickDetector = Cube and Cube:FindFirstChildOfClass("ClickDetector")
+        local orePosition = Cube.Position
+        local StatusText = Cube:WaitForChild("OreStatus"):WaitForChild("StatusText")
+
+        if string.find(StatusText.Text, "^矿物已被") then continue end
+
+        TelePort(orePosition)
+
+        fireclickdetector(ClickDetector, 10)
+
+        task.wait(getgenv().MiningRate or .5)
+    end
+end
+
+while getgenv().AutoMiningEnabled do
+    AutoMining()
+    task.wait(.1)
+end
