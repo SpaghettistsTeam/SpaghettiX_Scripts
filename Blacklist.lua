914b4da4d7bdbde570286c3e01b0e4424672e038
SpaghettistsTeam <uloverrs@outlook.com>
2026-02-27 01:18:11 +0800

    Update Blacklist.lua

--- Blacklist.lua
+++ Blacklist.lua
@@ -6,5 +6,5 @@ local Players = game:GetService("Players")
 local LocalPlayer = Players.LocalPlayer
 
 if table.find(Blacklist, LocalPlayer.UserId) then
-    Localplayer:Kick("您已被SpaghettistsTeam封禁。请加入QQ群 1084580131 解除封禁。（这或许是暂时的）")
+    LocalPlayer:Kick("您已被SpaghettistsTeam封禁。请加入QQ群 1084580131 解除封禁。（这或许是暂时的）")
 end
