03201ce03599326e4375ab426563c6ff78a4cf73
SpaghettistsTeam <uloverrs@outlook.com>
2026-02-27 22:57:53 +0800

    Update WhiteList.lua

--- WhiteList.lua
+++ WhiteList.lua
@@ -1,6 +1,6 @@
 --请在注入器中输入 setclipboard(game:GetService("RbxAnalyticsService"):GetClientId()) 来获取自己的hwid(hwid将会复制到你的剪贴板上)
 local Whitelist = {
-    "f32b9bce-b107-4619-90cc-123611964ee21",--Tips: 请将白名单玩家的hwid添加至这里（记得加逗号）
+    "f32b9bce-b107-4619-90cc-123611964ee2",--Tips: 请将白名单玩家的hwid添加至这里（记得加逗号）
     "764455FC-FCC0-4C5F-B9C8-BF5D0C1A4923",
     "0161191f-1521-475b-870f-53ef67029cf7",
     "445b91c3-5a5f-4a15-9628-b2eb64b4140e",
